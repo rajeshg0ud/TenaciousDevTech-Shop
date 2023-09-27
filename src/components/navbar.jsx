49241cf8ae37320671d9be0edc15956eb6e75c25
Rajesh Goud <rajeshgoud2314@gmail.com>
2023-09-27 12:55:23 +0530

    Add all project files

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,10 +8,10 @@ function Navbar(){
     <div className="navbar">  
         <div className="links">
             <Link className="linkH" to='/'>
-                <House size={29}/>
+                <House className="icon" size={29}/>
             </Link>
             <Link className="linkC" to='/cart' >
-                <ShoppingCart size={29} />
+                <ShoppingCart className="icon" size={29} />
                 <span className="iconName"> Cart</span>
             </Link>
             <a className="iconName" id="menu">⋮</a>
